9647b3e16574a5ba54b9c6953989e6a34f2ded8e
Vinicius <marcosviniciosguedesdossantos@gmail.com>
2025-04-16 23:24:30 -0300

    Atualização da documentação.

--- src/main/java/org/example/localizei/controller/UsuarioController.java
+++ src/main/java/org/example/localizei/controller/UsuarioController.java
@@ -1,16 +1,20 @@
 package org.example.localizei.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.example.localizei.dto.UsuarioRequestDto;
 import org.example.localizei.dto.UsuarioResponseDto;
+import org.example.localizei.exception.ApiErrorResponse;
 import org.example.localizei.service.UsuarioService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,15 +26,30 @@ public class UsuarioController {
 
     private final UsuarioService usuarioService;
 
-    @Operation(description = "Cria um usuário.")
+    @Operation(summary = "Cria um novo usuário")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Retorna o usuário que foi criado."),
-            @ApiResponse(responseCode = "400", description = "O endereço de E-mail inserido não é válido!")
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "Usuário criado com sucesso",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = UsuarioResponseDto.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Erro de validação dos dados enviados",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiErrorResponse.class)
+                    )
+            )
     })
     @PostMapping("/criar")
     public ResponseEntity<UsuarioResponseDto> criar(@Valid @RequestBody UsuarioRequestDto usuarioRequestDto) {
         UsuarioResponseDto usuarioResponseDto = usuarioService.criarUsuario(usuarioRequestDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(usuarioResponseDto);
     }
+
 
 }
---
+++ src/main/java/org/example/localizei/exception/ApiErrorResponse.java
@@ -0,0 +1,7 @@
+package org.example.localizei.exception;
+
+public class ApiErrorResponse {
+    public String mensagem;
+    public String campo;
+    public String dataHora;
+}
